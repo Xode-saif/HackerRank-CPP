f64d724fdd4f6099bf71a623a333abb82c58e437
Ambrish Dubey <dubey.ambrishdubey@gmail.com>
2023-07-30 23:59:51 +0530

    Add Input-and-Output.cpp file

---
+++ Input-and-Output.cpp
@@ -0,0 +1,11 @@
+// Link to the problem:- https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a, b, c;
+  cin >> a >> b >> c;
+  cout << a + b + c;
+  return 0;
+}
