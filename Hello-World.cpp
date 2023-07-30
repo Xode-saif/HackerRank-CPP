3c39faacd4bee836abe15d290331e925836e94f9
Md Saif Ali <81511126+Xode-saif@users.noreply.github.com>
2023-07-31 00:03:05 +0530

    Removed comma

--- Hello-World.cpp
+++ Hello-World.cpp
@@ -5,6 +5,6 @@ using namespace std;
 
 int main() {
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
-    cout<<"Hello, World!";
+    cout<<"Hello World!";
     return 0;
 }
