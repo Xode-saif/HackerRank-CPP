45673940d570dd0c8137b48e55fb349de505127d
Ambrish Dubey <dubey.ambrishdubey@gmail.com>
2023-07-30 23:49:37 +0530

    Add Hello-World.cpp file

---
+++ Hello-World.cpp
@@ -0,0 +1,10 @@
+// Link to Problem:- https://www.hackerrank.com/challenges/cpp-hello-world/problem?isFullScreen=true
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    cout<<"Hello, World!";
+    return 0;
+}
